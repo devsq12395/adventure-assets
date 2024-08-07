ddb53de7834a3dbeba01cdae4de817a6ce575cef
devsq12395 <seasonquest.mail@gmail.com>
2024-08-07 23:04:26 +0800

    updates

--- Scenes/Game/Scripts/UI/MainUI/MM_Inv2.cs
+++ Scenes/Game/Scripts/UI/MainUI/MM_Inv2.cs
@@ -10,15 +10,42 @@ public class MM_Inv2 : MonoBehaviour {
     public static MM_Inv2 I;
 	public void Awake(){ I = this; }
 
+	public GameObject go;
+	public CanvasGroup canvasGroup; // Add this for controlling alpha
+	public RectTransform imgWindow; // Add this for controlling the y position
+
+	public Image i_item;
+	public TextMeshProUGUI t_itemName, t_itemDesc;
+
 	public List<Button> itemsBTN;
 	public List<Inv2.Item> items;
 
 	public int page, pageMax, ITEMS_PER_PAGE;
 
+	public Inv2.Item itemSel;
+
 	public void setup (){
 		page = 1;
 		ITEMS_PER_PAGE = 8;
 	}
+
+	public void show (){
+        go.SetActive (true);
+
+        canvasGroup.alpha = 0f;
+        imgWindow.anchoredPosition = new Vector2(imgWindow.anchoredPosition.x, -300f);
+
+        imgWindow.DOAnchorPosY(0f, 0.2f).SetEase(Ease.OutBack);
+        canvasGroup.DOFade(1f, 0.2f);
+
+        itemSel = null;
+        setup_page ();
+    }
+
+    public void hide (){
+    	imgWindow.DOAnchorPosY(-300f, 0.2f).SetEase(Ease.InBack);
+        canvasGroup.DOFade(0f, 0.2f).OnComplete(() => go.SetActive(false));;
+    }
 
 	public void setup_page (){
 		pageMax = Inv2.I.get_max_pages (ITEMS_PER_PAGE);
@@ -34,7 +61,7 @@ public class MM_Inv2 : MonoBehaviour {
 		}
 	}
 
-	public void btn_item (int _ind){
+	public void btn_item (int _index){
 
 	}
 }
