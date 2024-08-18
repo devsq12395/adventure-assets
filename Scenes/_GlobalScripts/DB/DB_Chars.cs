298d039542b96ff703bdf056efd4e12d226e93e0
devsq12395 <seasonquest.mail@gmail.com>
2024-08-18 21:14:50 +0800

    updates

--- Scenes/_GlobalScripts/DB/DB_Chars.cs
+++ Scenes/_GlobalScripts/DB/DB_Chars.cs
@@ -20,6 +20,7 @@ public class DB_Chars : MonoBehaviour {
         public string name, nameUI, imgPort, equipWeapon;
         public string bioInfo, bioSkill1, bioSkill2;
         public int statHP, statAtk, statRange, statSkill, statSpeed, statArmor, statCritRate, statCritDam;
+        public int statScience, statMagic, statDriving, statEspionage, statComputers, statRepair, statLuck;
         public int goldCost;
 
         public CharData (string _name){
@@ -27,6 +28,7 @@ public class DB_Chars : MonoBehaviour {
             nameUI="";imgPort="";equipWeapon="";
             bioInfo="";bioSkill1="";bioSkill2="";
             statHP = 0; statAtk = 0; statRange = 0; statSkill = 0; statSpeed = 0;statArmor = 0; statCritRate = 0; statCritDam = 0;
+            statScience = 0; statMagic = 0; statDriving = 0; statEspionage = 0; statComputers = 0; statRepair = 0; statLuck = 0;
             goldCost=0;
         }
     }
@@ -38,7 +40,10 @@ public class DB_Chars : MonoBehaviour {
                 _new.nameUI="Tommy";
                 _new.imgPort="tommy";
                 _new.equipWeapon="gun";
-                _new.statHP = 8; _new.statAtk = 1; _new.statRange = 4; _new.statSkill = 1; _new.statSpeed = 1; _new.statArmor = 0; _new.statCritRate = 10; _new.statCritDam = 120;
+                _new.statHP = 8; _new.statAtk = 1; _new.statRange = 4; 
+                _new.statSkill = 1; _new.statSpeed = 1; _new.statArmor = 0; _new.statCritRate = 10; _new.statCritDam = 120;
+                _new.statScience = 4; _new.statMagic = 1; _new.statDriving = 7; 
+                _new.statEspionage = 4; _new.statComputers = 5; _new.statRepair = 5; _new.statLuck = 3;
                 _new.goldCost = 0;
 
                 _new.bioInfo = "";
@@ -50,6 +55,8 @@ public class DB_Chars : MonoBehaviour {
                 _new.imgPort="kazuma";
                 _new.equipWeapon="sword";
                 _new.statHP = 10; _new.statAtk = 2; _new.statRange = 3; _new.statSkill = 1; _new.statSpeed = 1; _new.statArmor = 0; _new.statCritRate = 15; _new.statCritDam = 140;
+                _new.statScience = 3; _new.statMagic = 2; _new.statDriving = 5; 
+                _new.statEspionage = 2; _new.statComputers = 4; _new.statRepair = 4; _new.statLuck = 5;
                 _new.goldCost = 0;
 
                 _new.bioInfo = "";
@@ -61,6 +68,8 @@ public class DB_Chars : MonoBehaviour {
                 _new.imgPort="anastasia";
                 _new.equipWeapon="sword";
                 _new.statHP = 8; _new.statAtk = 2; _new.statRange = 3; _new.statSkill = 1; _new.statSpeed = 1; _new.statArmor = 0; _new.statCritRate = 15; _new.statCritDam = 150;
+                _new.statScience = 5; _new.statMagic = 5; _new.statDriving = 2; 
+                _new.statEspionage = 2; _new.statComputers = 3; _new.statRepair = 3; _new.statLuck = 3;
                 _new.goldCost = 500;
 
                 _new.bioInfo = "";
@@ -72,6 +81,8 @@ public class DB_Chars : MonoBehaviour {
                 _new.imgPort="sylphine";
                 _new.equipWeapon="staff";
                 _new.statHP = 6; _new.statAtk = 1; _new.statRange = 6; _new.statSkill = 2; _new.statSpeed = 1; _new.statArmor = 0; _new.statCritRate = 15; _new.statCritDam = 140;
+                _new.statScience = 7; _new.statMagic = 8; _new.statDriving = 2; 
+                _new.statEspionage = 2; _new.statComputers = 5; _new.statRepair = 5; _new.statLuck = 2;
                 _new.goldCost = 500;
 
                 _new.bioInfo = "";
