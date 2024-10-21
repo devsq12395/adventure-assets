1e8e815762ad9104f5bc28e3350a8c2dddf32c54
devsq12395 <seasonquest.mail@gmail.com>
2024-10-21 21:00:31 +0800

    starting multiple maps

--- Scenes/Game/Scripts/Controllers/ContMap.cs
+++ Scenes/Game/Scripts/Controllers/ContMap.cs
@@ -11,10 +11,9 @@ public class ContMap : MonoBehaviour
     public DB_Maps.mapDetails details;
     public Dictionary<string, Vector2> pointList;
 
-    public delegate void create_map();
+    private List<GameObject> maps;
 
-    public void setup_map()
-    {
+    public void setup_map() {
         Grid[] allGrids = FindObjectsOfType<Grid>();
         foreach (Grid gridObject in allGrids) {
             if (gridObject != null){
@@ -30,14 +29,41 @@ public class ContMap : MonoBehaviour
         string _curMap = _maps[curMapLvl];
 
         details = DB_Maps.I.get_map_details(_curMap);
-
-        map = details.mapObj;
         pointList = details.pointList;
-
         ContEnemies.I.setup(_data.enemies[curMapLvl]);
 
         // Create a border around the map
         CreateMapBorder(details.size.x, details.size.y);
+
+        // Create the map game objects
+        create_map_objects (details.biome);
+    }
+
+    public void create_map_objects (string biome){
+        List<string> _mapGameObjectNames = DB_Maps.I.get_map_game_object (biome);
+        List<List<bool>> _mapMatrix = new List<List<bool>>();
+
+        // (0, 0) will be the uppermost part of the matrix
+        // Remember that _mapMatrix works this way: _mapMatrix [Y][X]
+        Vector2 _curPos = new Vector2 (0, 0);
+
+        while (_curPos.y <= details.mapObjMatrix_sizeY){
+            // Add a tile to current position if empty
+            if (!_mapMatrix [_curPos.y][_curPos.x]) {
+                string _newGameObj = _mapGameObjectNames [Random.Range (0, _mapGameObjectNames.Count)];
+
+                DB_Maps.mapObject _mapObj = DB_Maps.I.get_map_game_object (_newGameObj);
+
+                _mapObj.go.transform.position = new Vector2 ();
+                // Set _mapMatrix value here depending on the size of the added _mapObj
+            }
+
+            // Check next position
+            _curPos.x++;
+            if (_curPos.x > details.mapObjMatrix_sizeX) {
+                _curPos.y++;
+            }
+        }
     }
 
     private void CreateMapBorder(float width, float height)
--- Scenes/Game/Scripts/Database/DB_Maps.cs
+++ Scenes/Game/Scripts/Database/DB_Maps.cs
@@ -11,16 +11,29 @@ public class DB_Maps : MonoBehaviour {
     public GameObject goMap_woosterSquare1, goMap_woosterSquare2, goMap_woosterSquare3, goMap_woosterSquare4, goMap_trainingGrounds;
 
     public struct mapDetails {
-        public string name;
+        public string name, biome;
         public Dictionary<string, Vector2> pointList;
         public GameObject mapObj;
         public Vector2 size;
+        public int mapObjMatrix_sizeX, mapObjMatrix_sizeY;
 
         public mapDetails (string _name){
             pointList = new Dictionary<string, Vector2> ();
             name = _name;
+            biome = "";
             mapObj = null;
             size = new Vector2 (0, 0);
+            mapObjMatrix_sizeX = 0; mapObjMatrix_sizeY = 0;
+        }
+    }
+
+    public struct mapObject {
+        public GameObject go;
+        public int sizeX, sizeY;
+
+        public mapObject (GameObject _go){
+            go = _go;
+            sizeX = 1; sizeY = 1;
         }
     }
 
@@ -29,36 +42,12 @@ public class DB_Maps : MonoBehaviour {
 
         switch (_name) {
             // Wooster Square 1
-            case "map-tutorial": 
+            case "map-tutorial": case "map-wooster-square-1": case "map-wooster-square-2": case "map-wooster-square-3": case "map-wooster-square-4": 
                 _new = get_map_details_generic (_new);
-                _new.mapObj = GameObject.Instantiate (goMap_woosterSquare1, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
-                break;
-            case "map-wooster-square-1": 
-                _new = get_map_details_generic (_new);
-                _new.mapObj = GameObject.Instantiate (goMap_woosterSquare1, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
-                break;
-            case "map-wooster-square-2": 
-                _new = get_map_details_generic (_new);
-                _new.mapObj = GameObject.Instantiate (goMap_woosterSquare2, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
-                break;
-            case "map-wooster-square-3": 
-                _new = get_map_details_generic (_new);
-                _new.mapObj = GameObject.Instantiate (goMap_woosterSquare3, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
-                break;
-            case "map-wooster-square-4": 
-                _new = get_map_details_generic (_new);
-                _new.mapObj = GameObject.Instantiate (goMap_woosterSquare4, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
                 break;
             
             case "woosterSquare_rand":
                 _new = get_map_details_generic (_new);
-                switch (Random.Range (0, 3)) {
-                    case 0: _new.mapObj = GameObject.Instantiate (goMap_woosterSquare1, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;  break;
-                    case 1: _new.mapObj = GameObject.Instantiate (goMap_woosterSquare2, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; break;
-                    case 2: _new.mapObj = GameObject.Instantiate (goMap_woosterSquare3, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; break;
-                    default:_new.mapObj = GameObject.Instantiate (goMap_woosterSquare4, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; break;
-                }
-                SceneManager.MoveGameObjectToScene(_new.mapObj, SceneManager.GetSceneByName("Game"));
                 break;
 
             case "training-grounds":
@@ -70,22 +59,62 @@ public class DB_Maps : MonoBehaviour {
         return _new;
     }
 
+    public List<string> get_map_lists (string _biome){
+        List<string> _new = new List<string>();
+        switch (_biome){
+            case "wooster-square":
+                _new.AddRange (new string []{"map-wooster-square-1","map-wooster-square-2","map-wooster-square-3","map-wooster-square-4"});
+                break;
+        }
+        return _new;
+    }
+
+    public mapObject get_map_game_object (string _name) {
+        mapObject _new = new mapObject(null);
+
+        switch (_name) {
+            case "map-wooster-square-1": 
+                _new.go = GameObject.Instantiate (goMap_woosterSquare1, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
+                _new.sizeX = 1; _new.sizeY = 3;
+                break;
+            case "map-wooster-square-2": 
+                _new.go = GameObject.Instantiate (goMap_woosterSquare2, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
+                _new.sizeX = 1; _new.sizeY = 1;
+                break;
+            case "map-wooster-square-3": 
+                _new.go = GameObject.Instantiate (goMap_woosterSquare3, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
+                _new.sizeX = 1; _new.sizeY = 1;
+                break;
+            case "map-wooster-square-4": 
+                _new.go = GameObject.Instantiate (goMap_woosterSquare4, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject; 
+                _new.sizeX = 1; _new.sizeY = 1;
+                break;
+
+        }
+
+        SceneManager.MoveGameObjectToScene(_new, SceneManager.GetSceneByName("Game"));
+        return _new;
+    }
+
     public mapDetails get_map_details_generic (mapDetails _new){
         _new.size = new Vector2 (18, 18);
+        
+        _new.biome = "wooster-square";
+        _new.mapObjMatrix_sizeX = 5; _new.mapObjMatrix_sizeY = 5;
 
         _new.pointList.Add ("playerSpawn", new Vector2 (0, 0));
-        _new.pointList.Add ("playerLounge", new Vector2 (-500, -500));
+        _new.pointList.Add ("playerLounge", new Vector2 (-5000, -5000));
 
         return _new;
     }
     public mapDetails get_map_details_training_grounds (mapDetails _new){
         _new.size = new Vector2 (18, 18);
 
-        _new.pointList.Add ("playerSpawn", new Vector2 (0, 0));
-        _new.pointList.Add ("playerLounge", new Vector2 (-500, -500));
+        _new.biome = "wooster-square";
+        _new.mapObjMatrix_sizeX = 5; _new.mapObjMatrix_sizeY = 5;
 
-        _new.mapObj = GameObject.Instantiate (goMap_trainingGrounds, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
-        SceneManager.MoveGameObjectToScene(_new.mapObj, SceneManager.GetSceneByName("Game"));
+        _new.pointList.Add ("playerSpawn", new Vector2 (0, 0));
+        _new.pointList.Add ("playerLounge", new Vector2 (-5000, -5000));
 
         return _new;
     }
