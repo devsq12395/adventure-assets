8129c74828b6c5e8ba491e807a9a6412d8de7fe1
devsq12395 <seasonquest.mail@gmail.com>
2024-11-02 20:39:46 +0800

    creating shadows

--- Scenes/Game/Scripts/Objects/InGameObject.cs
+++ Scenes/Game/Scripts/Objects/InGameObject.cs
@@ -10,8 +10,6 @@ public class InGameObject : MonoBehaviour {
     public string nameUI, type;
     
     [Header("Types: unit, missile, collect. Put dummies as missile")]
-        // TYPES: unit, missile, collect
-        // To be set in Unity GameObject's component
     public int owner;
     public int id;
 
@@ -44,12 +42,12 @@ public class InGameObject : MonoBehaviour {
     public Dictionary<string, string> equipItems;
 
     // Movement
-    public Vector2 curPos = new Vector2 (0, 0);
-    public Vector2 movInput = new Vector2 (0, 0);
-    public Vector2 nxtPos = new Vector2 (0, 0);
+    public Vector2 curPos = new Vector2(0, 0);
+    public Vector2 movInput = new Vector2(0, 0);
+    public Vector2 nxtPos = new Vector2(0, 0);
     public string facing;
     public bool isWalk;
-    public Vector2 walkTargPos = new Vector2 (0, 0);
+    public Vector2 walkTargPos = new Vector2(0, 0);
     public bool checkBorder = true;
 
     // Move to ang
@@ -57,7 +55,7 @@ public class InGameObject : MonoBehaviour {
     public float constMovAng_spd, constMovAng_ang, constMovAng_dur, constMovAng_dist, propellDist;
     public Vector2 constMovAng_movTarget;
     public string propellType = "";
-    public Vector2 propellFirstPos = new Vector2 (0, 0);
+    public Vector2 propellFirstPos = new Vector2(0, 0);
     
     // Move to dir
     public bool constMovDir_isOn = false;
@@ -85,24 +83,34 @@ public class InGameObject : MonoBehaviour {
     public int summonedBy;
 
     // Buffs
-    public List <ContBuffs.buff> buffs;
+    public List<ContBuffs.buff> buffs;
 
     // Skills
-    public List <SkillTrig> skills;
+    public List<SkillTrig> skills;
 
     // UI
     public HealthBarScript barHP, barSta;
 
+    [Header("Shadow Settings")]
+    private Vector2 shadowOffset;
+    private Vector2 shadowScaleMultiplier;
+    private Color shadowColor;
+
+    private GameObject shadow; // The shadow object
+    private SpriteRenderer shadowRenderer; // SpriteRenderer for the shadow
+
     void Start() {
-        renderer = GetComponent <Renderer> ();
+        renderer = GetComponent<Renderer>();
 
-        buffs = new List<ContBuffs.buff> ();
-        skills = new List<SkillTrig> ();
-
+        // Initialize components
+        buffs = new List<ContBuffs.buff>();
+        skills = new List<SkillTrig>();
         equipItems = new Dictionary<string, string>();
+
+        // Equip items if hero
         if (tags.Contains("hero")) {
             foreach (string equipSlot in Inv2.I.equipStrList) {
-                equipItems[equipSlot] = Inv2.I.get_item_name_in_equipped_slot (equipSlot, name);
+                equipItems[equipSlot] = Inv2.I.get_item_name_in_equipped_slot(equipSlot, name);
             }
         } else {
             foreach (string equipSlot in Inv2.I.equipStrList) {
@@ -110,36 +118,74 @@ public class InGameObject : MonoBehaviour {
             }
         }
 
-        rb = GetComponent <Rigidbody2D> ();
-        anim = GetComponent <Animator> ();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         hasAnim = (anim != null);
 
+        // Set ID and default skills
         id = ContObj.I.curID;
         ContObj.I.curID++;
+        ContObj.I.set_default_skills(this);
 
-        ContObj.I.set_default_skills (this);
-        
+        // Set shadow variables
+        shadowOffset = new Vector2(0f, -0.15f); // Offset to adjust shadow position
+        shadowScaleMultiplier = new Vector2(1.8f, 0.5f); // Scale multiplier for shadow
+        shadowColor = new Color(0, 0, 0, 0.4f); // Darker shadow color for visibility
+
+        // Find the shadow child object
+        Transform shadowTransform = transform.Find("Shadow");
+        if (shadowTransform != null) {
+            shadow = shadowTransform.gameObject;
+            shadowRenderer = shadow.GetComponent<SpriteRenderer>();
+
+            if (shadowRenderer != null) {
+                // Apply initial color and scale settings
+                shadowRenderer.color = shadowColor;
+                shadow.transform.localPosition = shadowOffset;
+                UpdateShadowScale();
+            } else {
+                Debug.LogWarning("Shadow GameObject is missing a SpriteRenderer component.");
+            }
+        } else {
+            Debug.LogWarning("Shadow GameObject not found. Please ensure a child object named 'Shadow' is added.");
+        }
+
         InvokeRepeating("update_every_10th_ms", 0.5f, 0.1f);
     }
 
     void Update() {
-        ContObj.I.update_obj (this);
-    }
-    
-    private void update_every_10th_ms (){
-        ContObj.I.update_every_10th_ms (this);
+        ContObj.I.update_obj(this);
+        UpdateShadowPositionAndScale();
     }
 
-    public virtual void change_anim (string _anim) {
-        
+    private void UpdateShadowPositionAndScale() {
+        if (shadow != null) {
+            // Update shadow position and scale based on the game object's scale
+            shadow.transform.localPosition = shadowOffset;
+            UpdateShadowScale();
+        }
     }
 
-    // Collision
-    public void OnTriggerStay2D (Collider2D _collision){
-        InGameObject _obj2_scrpt = _collision.gameObject.GetComponent <InGameObject> ();
+    private void UpdateShadowScale() {
+        // Scale the shadow based on the game object's current scale and the shadowScaleMultiplier
+        Vector2 scaledShadowSize = new Vector2(
+            transform.localScale.x * shadowScaleMultiplier.x,
+            transform.localScale.y * shadowScaleMultiplier.y
+        );
+        shadow.transform.localScale = scaledShadowSize;
+    }
+
+    private void update_every_10th_ms() {
+        ContObj.I.update_every_10th_ms(this);
+    }
+
+    // Other methods remain the same...
+
+    public void OnTriggerStay2D(Collider2D _collision) {
+        InGameObject _obj2_scrpt = _collision.gameObject.GetComponent<InGameObject>();
 
         if (_obj2_scrpt != null) {
-            ContCollision.I.collision (this, _obj2_scrpt);
+            ContCollision.I.collision(this, _obj2_scrpt);
         }
     }
 }
